66fc356529d87b9f747908fb259155b8e5fbb252
kyle <kyleeng003@gmail.com>
2023-06-07 14:35:14 -0400

    created new project to make language dart instead of java

--- android/app/src/main/java/com/example/light_weight/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.light_weight;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:light_weight/main.dart';
+import 'package:lightweight/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
