e5e3e0146410e8afe5458051fb27c4bee7939068
kyle <kyleeng003@gmail.com>
2023-06-15 21:07:11 -0400

    Updated documentation for main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,34 @@
+/*
+ * -------------------------------------------------------------------------------------
+ *  _      _       _     _    __          __  _       _     _
+ * | |    (_)     | |   | |   \ \        / / (_)     | |   | |       /\
+ * | |     _  __ _| |__ | |_   \ \  /\  / /__ _  __ _| |__ | |_     /  \   _ __  _ __
+ * | |    | |/ _` | '_ \| __|   \ \/  \/ / _ \ |/ _` | '_ \| __|   / /\ \ | '_ \| '_ \
+ * | |____| | (_| | | | | |_     \  /\  /  __/ | (_| | | | | |_   / ____ \| |_) | |_) |
+ * |______|_|\__, |_| |_|\__|     \/  \/ \___|_|\__, |_| |_|\__| /_/    \_\ .__/| .__/
+ *            __/ |                              __/ |                    | |   | |
+ *           |___/                              |___/                     |_|   |_|
+ *
+ * -------------------------------------------------------------------------------------
+ * Application Function:
+ *  This applications functions as an interface for users to create and track previous and future workouts
+ *    The app will allow for users to develop a new routine in the gym or fortify an existing workout routine
+ *
+ * Key Features:
+ *  - Premade Exercises
+ *  - Premade Workouts
+ *  - Exercise log
+ *  - Workout builder
+ *  - Workout log
+ *  - Set timer
+ *  - Data Persistence
+ *
+ * Optional Features:
+ *  - Workout suggestions
+ *  - Progress projection
+ *  - Accounts
+ *
+ */
 import 'package:flutter/material.dart';
 
 void main() {
