328fbef05d00d67ca97e56cb150ac19aa3b64e03
KEGit28 <Kyleeng003@gmail.com>
2023-06-19 19:45:21 -0400

    Added the ability to navigate to a new page while keeping the context of which button was pushed

--- lib/main.dart
+++ lib/main.dart
@@ -31,38 +31,69 @@
  */
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Light Weight',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(title: 'Light Weight Home'),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key, required this.title});
+
+  final String title;
+
 
   @override
   Widget build(BuildContext context) {
-    const title = 'Workout List';
-
-    return MaterialApp(
-      title: title,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(title),
-        ),
-        body: GridView.count(
-          // Create a grid with 2 columns. If you change the scrollDirection to
-          // horizontal, this produces 2 rows.
-          crossAxisCount: 2,
-          // Generate 15 widgets that display their index in the List.
-          children: List.generate(15, (index) {
-            return Center(
-              child: ElevatedButton(
-                child: Text('Workout ${index+1}'),
-                onPressed: () {
-                },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: GridView.count(
+        // Create a grid with 2 columns. If you change the scrollDirection to
+        // horizontal, this produces 2 rows.
+        crossAxisCount: 2,
+        // Generate 15 widgets that display their index in the List.
+        children: List.generate(15, (index) {
+          return Center(
+            child: ElevatedButton(
+              child: Text('Workout ${index + 1}'),
+              onPressed: () {
+                Navigator.push(
+                context, MaterialPageRoute(
+                builder: (context) => WorkoutPage(number: index+1),
               ),
-            );
-          }),
+              );},
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
+
+class WorkoutPage extends StatelessWidget {
+  WorkoutPage({super.key, required this.number});
+  final int number;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+        title: Text('Workout ${this.number}'),
         ),
+        body: Center(
+        child: Text('Workout Here')
       ),
     );
   }
