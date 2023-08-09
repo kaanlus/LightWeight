f009fabb02f45e96e5954942cd1738641de4949d
KEGit28 <Kyleeng003@gmail.com>
2023-08-08 21:32:07 -0400

    Created goal UI

--- lib/pages/GoalPage.dart
+++ lib/pages/GoalPage.dart
@@ -34,12 +34,130 @@ class GoalPageState extends State<GoalPage> {
       body: Stack(
         children : [
           const Align(
-          alignment: Alignment.topCenter,
-          child:
+            alignment: Alignment.topCenter,
+            child:
+            Padding(
+              padding: EdgeInsets.only(top: 30.0),
+              child: Text('Goals',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+              ),
+            ),
+          ),
+
+          Align(
+            alignment: Alignment.topCenter,
+            child:
+            Padding(
+              padding: const EdgeInsets.only(top: 80.0 ),
+              child:
+              ElevatedButton( child: const Text('\t\t\tCreate New Goal\t\t\t'),
+                  onPressed: () {
+
+                  }
+              ),
+            ),
+          ),
+
+          const Align(
+            alignment: Alignment.topCenter,
+            child:
+            Padding(
+              padding: EdgeInsets.only(top: 140.0),
+              child: Text('Active Goals',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+              ),
+            ),
+          ),
+
           Padding(
-            padding: EdgeInsets.only(top: 30.0),
-            child: Text('Goals',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),),),),
+            padding: const EdgeInsets.only(top: 190, left: 15, right: 15, bottom: 360),
+            child:
+            ListView.builder(
+                itemCount: 10/*pro.Goals.length*/,
+                itemBuilder: (BuildContext context, int index) {
+                return Row(
+                  children: [
+                    const Icon(Icons.check_box_outline_blank,
+                      color: Colors.red,
+                      size: 50,),
+
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10, bottom: 10),
+                      child:
+                      SizedBox(
+                        width: 200,
+                        child:
+                        TextButton(
+                          child: Text('Goal place holder',
+                            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black),),
+                          onPressed: () {
+
+                          },
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 120,
+                      child:
+                      Padding(
+                        padding: const EdgeInsets.only(left: 5),
+                        child:
+                        ElevatedButton(
+                            child: Text('Complete Goal',
+                              textAlign: TextAlign.center,),
+                        onPressed: () {
+
+                        }),
+                      ),
+                    ),
+                  ],
+                );
+              }
+              ),
+            ),
+
+          const Align(
+            alignment: Alignment.topCenter,
+            child:
+            Padding(
+              padding: EdgeInsets.only(top: 450.0),
+              child: Text('Completed Goals',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(top: 500, left: 15, right: 15, bottom: 40),
+            child:
+            ListView.builder(
+                itemCount: 10/*pro.Goals.length*/,
+                itemBuilder: (BuildContext context, int index) {
+                  return Row(
+                    children: [
+                      const Icon(Icons.check_box_outlined,
+                        color: Colors.red,
+                        size: 50,),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10, bottom: 10),
+                        child:
+                        SizedBox(
+                          width: 200,
+                          child:
+                          TextButton(
+                            child: Text('Goal place holder',
+                              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black),),
+                            onPressed: () {
+
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                }
+            ),
+          ),
         ]
       ),
     );
